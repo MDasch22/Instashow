4b28abb57376ea2f6b59ae39fc0960b0763ab120
dasch22 <michaeldasch22@gmail.com>
2022-08-12 22:42:07 -0700

    drop down navbar

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector} from 'react-redux'
 import { NavLink } from 'react-router-dom';
-import LogoutButton from '../auth/LogoutButton';
 import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import { logout } from '../../store/session';
 import './navbar.css'
 
 
 const NavBar = () => {
+  const dispatch = useDispatch()
+
   const sessionUser = useSelector(state => state.session.user)
 
+  const [open, setOpen] = useState(false)
+
+  const onLogout = async() => {
+    await dispatch(logout())
+  }
 
   return (
     <nav className='nav'>
@@ -42,13 +49,29 @@ const NavBar = () => {
               </NavLink>
             </li>
             <li id="nav-bar-bttns">
-              <NavLink to={`/${sessionUser.username}`} exact={true} activeClassName='active'>
-                <img src={sessionUser.profile_pic} id="nav-bar-profile-bttn"style={{width: 30, height: 30}}></img>
-              </NavLink>
+              <>
+                <img onClick={() => setOpen(!open)} src={sessionUser.profile_pic} id="nav-bar-profile-bttn"style={{width: 30, height: 30}}></img>
+                {open && (
+                <>
+                  <div id='pointer-arrow'>  </div>
+                  <div className='dropDown'>
+                    <NavLink id="dropdown-item" to={`/${sessionUser.username}`} onClick={() => setOpen(!open)}>
+                      <i id="profile-icon" className="fa-regular fa-circle-user "></i>
+                      <p id="dropdown-profile"> My Profile </p>
+                    </NavLink>
+                    <NavLink id="dropdown-item" to={`/${sessionUser.username}/edit`} onClick={() => setOpen(!open)}>
+                      <i className="fa-solid fa-gears"></i>
+                      <p id="dropdown-text"> Manage account </p>
+                    </NavLink>
+                    <div id='dropdown-logout' onClick={onLogout}>
+                      Logout
+                    </div>
+                  </div>
+                </>
+                )}
+              </>
             </li>
-            <li id="nav-bar-bttns">
-              <LogoutButton />
-            </li>
+
           </li>
         </>
         )}
