2a31d1320dcdde37a0acf9d0fe92d691f0aabdc4
dasch22 <michaeldasch22@gmail.com>
2022-08-14 02:33:03 -0700

    footerchange

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -7,7 +7,7 @@ export default function Footer() {
     <div className='footer-nav-container'>
       <div className='footer-content-top'>
         <img id="footer-dasch-img" src="https://instashowbucket.s3.us-west-1.amazonaws.com/coaching.jpeg" style={{width:30, height:30}}></img>
-        <p id="michael-dasch"> Michael Dasch </p>
+        <p id="michael-dasch">Developer: Michael Dasch </p>
         <a id="anchor" target="_blank" href='https://www.linkedin.com/in/michael-dasch-71b6a6187/'> <i className="fa-brands fa-linkedin fa-xl"></i></a>
         <a id="anchor" target="_blank" href='https://github.com/MDasch22'><i className="fa-brands fa-square-github fa-xl"></i></a>
       </div>
