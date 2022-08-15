2ec1589d897606eeb759ddb07ef1a3b097ddf872
dasch22 <michaeldasch22@gmail.com>
2022-08-15 01:38:31 -0700

    comment seeder fix

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -2,36 +2,36 @@ from app.models import  db, Comment
 
 def seed_comments():
   comment_p1_1 = Comment(
-    user_id = 2, post_id = 1, comment="Woooow, thats so awesome!! So Jelous!"
+    user_id = 2, post_id = 11, comment="Woooow, thats so awesome!! So Jelous!"
   )
   comment_p1_2 = Comment(
-    user_id = 3, post_id = 1, comment=("Looks insane, take me with you next time!")
+    user_id = 3, post_id = 11, comment=("Looks insane, take me with you next time!")
   )
   # -------------------------
   comment_p2_1 = Comment(
-    user_id = 1, post_id = 2, comment=("I love how green it looks there, booking my flight now.")
+    user_id = 1, post_id = 16, comment=("I love how green it looks there, booking my flight now.")
   )
   comment_p2_2 = Comment(
-    user_id = 3, post_id = 2, comment=("It almost looks fake thats how pretty it looks!")
+    user_id = 3, post_id = 16, comment=("It almost looks fake thats how pretty it looks!")
   )
   # -------------------------
   comment_p3_1 = Comment(
-    user_id = 1, post_id = 3, comment=("I've always wanted to go to japan, seeing this makes me want to go even more now")
+    user_id = 1, post_id = 21, comment=("I've always wanted to go to japan, seeing this makes me want to go even more now")
   )
   # -------------------------
   comment_p4_1= Comment(
-    user_id = 2, post_id = 4, comment=("Aww, cuddling with the big boy is never a bad move.")
+    user_id = 2, post_id = 25, comment=("Aww, cuddling with the big boy is never a bad move.")
   )
   # -------------------------
   comment_p5_1 = Comment(
-    user_id = 3, post_id = 5, comment=("Totally rad shot, how were the waves today?")
+    user_id = 3, post_id = 30, comment=("Totally rad shot, how were the waves today?")
   )
   comment_p5_2 = Comment(
-    user_id = 2, post_id = 5, comment=('The waves were siiiick this morning, you should of came through!')
+    user_id = 2, post_id = 30, comment=('The waves were siiiick this morning, you should of came through!')
   )
   # -------------------------
   comment_p6_1 = Comment(
-    user_id = 1, post_id = 6, comment=("Save travels amigo!")
+    user_id = 1, post_id = 32, comment=("Save travels amigo!")
   )
 
   db.session.add(comment_p1_1)
