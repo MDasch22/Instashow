dede4128191415fe5142fc8e079319100404bf1e
dasch22 <michaeldasch22@gmail.com>
2022-08-24 15:27:30 -0700

    more post by on single post

--- react-app/src/components/SinglePost/index.js
+++ react-app/src/components/SinglePost/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { NavLink, useHistory, useParams } from 'react-router-dom'
 import Modal from 'react-modal'
-import { thunkGetSinglePost, thunkDeletePost } from '../../store/posts'
+import { thunkGetSinglePost, thunkDeletePost, thunkLoadAllPosts} from '../../store/posts'
 import EditPost from '../EditPost'
 import CreateCommentForm from '../CreateComment'
 import { thunkGetComments } from '../../store/comment'
@@ -11,6 +11,7 @@ import LikeButton from '../LikesButton'
 
 
 import './singlepost.css'
+import ExplorePagePost from '../ExplorePagePostCard'
 
 
 export default function SinglePost() {
@@ -18,7 +19,7 @@ export default function SinglePost() {
   const history = useHistory()
   const { post_id } = useParams()
 
-  const post = useSelector(state => state.post[post_id])
+  const posts = useSelector(state => state.post)
   const comments = useSelector(state => Object.values(state.comment))
   const sessionUser = useSelector(state => state.session.user)
 
@@ -26,9 +27,16 @@ export default function SinglePost() {
   const [showEditPost, setShowEditPost] = useState(false)
   const [submitted , setSubmitted] = useState(false)
 
+  const post = posts[post_id]
+
+  const allPosts = Object.values(posts)
+  const usersPosts = allPosts.filter(daPost => daPost.owner.id === post.owner.id)
+  const usersPost = usersPosts.filter(singPost => singPost.id !== post.id)
+
+
   useEffect(() => {
     window.scroll(0,0)
-    dispatch(thunkGetSinglePost(post_id))
+    dispatch(thunkLoadAllPosts())
     dispatch(thunkGetComments(post_id))
   },[dispatch, post_id])
 
@@ -146,90 +154,110 @@ if(!post) return null
 if(!comments) return null
 
   return (
-    <div className='single-post'>
-      <div className='single-post-container'>
-        <img src={post.image} style={{width: 600 ,height: 600}} alt='post-image'></img>
-        <div className='single-post-interation'>
-          <div className='single-post-profilepic-username'>
-            <NavLink id='link-to-ownerProfile' to={`/${post.owner.username}`}>
-              <img id='single-post-profilePic' src={post.owner.profile_pic} style={{width: 40 ,height: 40}} alt='post-profile-pic'></img>
-              <div id='single-post-username'>{post.owner.username}</div>
-            </NavLink>
-            {sessionUser.id === post.owner.id &&
-              <div>
-                <button id="ellipsis-post" onClick={openEllipsis}> <i className="fa-solid fa-ellipsis"></i> </button>
-                <Modal className='single-post-edit-modal' isOpen={showEllipsis} style={formStyles2}>
-                  <button id="single-post-delete" onClick={onDelete}> Delete </button>
-                  <button id="single-post-edit" onClick={openEditPostForm}>Edit</button>
-                </Modal>
-                <Modal isOpen={showEditPost} style={formStyles}>
-                  <EditPost setTrigger={closeEditPostForm}/>
-                </Modal>
-              </div>
-            }
-          </div>
-          <div className='border-bottom-single-post'> </div>
-          <div className='single-post-comment-section'>
-            <div className='post-caption'>
-              <NavLink id='link-caption-user'to={`/${post.owner.username}`}>
-                <img id='user-post-profilePic' src={post.owner.profile_pic} style={{width: 30 ,height: 30}} alt='post-profile-pic'></img>
+    <div>
+      <div className='single-post'>
+        <div className='single-post-container'>
+          <img src={post.image} style={{width: 600 ,height: 600}} alt='post-image'></img>
+          <div className='single-post-interation'>
+            <div className='single-post-profilepic-username'>
+              <NavLink id='link-to-ownerProfile' to={`/${post.owner.username}`}>
+                <img id='single-post-profilePic' src={post.owner.profile_pic} style={{width: 40 ,height: 40}} alt='post-profile-pic'></img>
+                <div id='single-post-username'>{post.owner.username}</div>
               </NavLink>
-              <p id='post-owner-username'>{post.owner.username}</p>
-              <p id='user-post-caption'>{post.caption}</p>
+              {sessionUser.id === post.owner.id &&
+                <div>
+                  <button id="ellipsis-post" onClick={openEllipsis}> <i className="fa-solid fa-ellipsis"></i> </button>
+                  <Modal className='single-post-edit-modal' isOpen={showEllipsis} style={formStyles2}>
+                    <button id="single-post-delete" onClick={onDelete}> Delete </button>
+                    <button id="single-post-edit" onClick={openEditPostForm}>Edit</button>
+                  </Modal>
+                  <Modal isOpen={showEditPost} style={formStyles}>
+                    <EditPost setTrigger={closeEditPostForm}/>
+                  </Modal>
+                </div>
+              }
             </div>
-            <div>
-              {comments.map(comment => {
-                return(
-                  <div className='single-post-single-comment'>
-                    <Comments postId={post_id} comment={comment}/>
-                  </div>
+            <div className='border-bottom-single-post'> </div>
+            <div className='single-post-comment-section'>
+              <div className='post-caption'>
+                <NavLink id='link-caption-user'to={`/${post.owner.username}`}>
+                  <img id='user-post-profilePic' src={post.owner.profile_pic} style={{width: 30 ,height: 30}} alt='post-profile-pic'></img>
+                </NavLink>
+                <p id='post-owner-username'>{post.owner.username}</p>
+                <p id='user-post-caption'>{post.caption}</p>
+              </div>
+              <div>
+                {comments.map(comment => {
+                  return(
+                    <div className='single-post-single-comment'>
+                      <Comments postId={post_id} comment={comment}/>
+                    </div>
+                  )}
                 )}
-              )}
+              </div>
             </div>
-          </div>
-          <div className='border-bottom-single-post'> </div>
-          <div className='single-post-likes-container'>
-            <LikeButton post={post}/>
-            <div className='single-post-liked-by'>
-              {post.likes.length === 0 ?
-                  <div className='hidden'> No likes yet</div>
-                  :
-                  <p>{post.likes.length === 1 ?
-                        <div className='liked-by-container'>
-                          <img id='liked-by-pic' src={post.likes[0].profile_pic} style={{height:20, width: 20}}></img>
-                          <p id='liked-by-usernames'>Liked by {post.likes[0].username} </p>
-                        </div>
-                      :
-                        <div>
-                          {post.likes.length === 2 ?
-                            <div className='liked-by-container'>
-                              <img id='liked-by-pic' src={post.likes[0].profile_pic} style={{height:20, width: 20}}></img>
-                              <div id='overlapping-pic'>
-                                <img id='overlapping-by-pic' src={post.likes[1].profile_pic} style={{height:20, width: 20}}></img>
+            <div className='border-bottom-single-post'> </div>
+            <div className='single-post-likes-container'>
+              <LikeButton post={post}/>
+              <div className='single-post-liked-by'>
+                {post.likes.length === 0 ?
+                    <div className='hidden'> No likes yet</div>
+                    :
+                    <p>{post.likes.length === 1 ?
+                          <div className='liked-by-container'>
+                            <img id='liked-by-pic' src={post.likes[0].profile_pic} style={{height:20, width: 20}}></img>
+                            <p id='liked-by-usernames'>Liked by {post.likes[0].username} </p>
+                          </div>
+                        :
+                          <div>
+                            {post.likes.length === 2 ?
+                              <div className='liked-by-container'>
+                                <img id='liked-by-pic' src={post.likes[0].profile_pic} style={{height:20, width: 20}}></img>
+                                <div id='overlapping-pic'>
+                                  <img id='overlapping-by-pic' src={post.likes[1].profile_pic} style={{height:20, width: 20}}></img>
+                                </div>
+                                <p id='liked-by-usernames'>Liked by {post.likes[0].username} and 1 other</p>
+                              </div >
+                              :
+                              <div className='liked-by-container'>
+                                <img id='liked-by-pic' src={post.likes[0].profile_pic} style={{height:20, width: 20}}></img>
+                                <div id='overlapping-pic'>
+                                  <img id='overlapping-by-pic' src={post.likes[1].profile_pic} style={{height:20, width: 20}}></img>
+                                </div>
+                                <p id='liked-by-usernames'>Liked by {post.likes[0].username} and {post.likes.length - 1} others </p>
                               </div>
-                              <p id='liked-by-usernames'>Liked by {post.likes[0].username} and 1 other</p>
-                            </div >
-                            :
-                            <div className='liked-by-container'>
-                              <img id='liked-by-pic' src={post.likes[0].profile_pic} style={{height:20, width: 20}}></img>
-                              <div id='overlapping-pic'>
-                                <img id='overlapping-by-pic' src={post.likes[1].profile_pic} style={{height:20, width: 20}}></img>
-                              </div>
-                              <p id='liked-by-usernames'>Liked by {post.likes[0].username} and {post.likes.length - 1} others </p>
-                            </div>
-                          }
-                        </div>
-                      }
-                  </p>
-                }
+                            }
+                          </div>
+                        }
+                    </p>
+                  }
+              </div>
             </div>
-          </div>
-          <div className='post-created'>{post.created_at.split(' ').slice(0, 4).join(' ')}</div>
-          <div className='border-bottom-single-post'> </div>
-          <div>
-            <CreateCommentForm postId={post_id}/>
+            <div className='post-created'>{post.created_at.split(' ').slice(0, 4).join(' ')}</div>
+            <div className='border-bottom-single-post'> </div>
+            <div>
+              <CreateCommentForm postId={post_id}/>
+            </div>
           </div>
         </div>
+      </div>
+      <div className='more-posts'>
+        {usersPost.length > 0 ?
+          <div className='more-posts-section'>
+            <p id="more-post-by">More post by <b>{post.owner.username}</b></p>
+            <div id="more-post-images">
+              {usersPost.slice(0, 4).map(post => {
+                return (
+                  <div>
+                     <ExplorePagePost post={post}/>
+                    </div>
+                  )
+                })}
+            </div>
+          </div>
+          :
+          null
+        }
       </div>
     </div>
   )
